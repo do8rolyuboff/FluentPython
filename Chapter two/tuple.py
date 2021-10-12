fb8a83c5ecb30f6334ff7b062c974e12ed2fe02d
do8rolyuboff <dobniknik@gmail.com>
2021-10-12 23:14:53 +0300

    Update tuple.py
    Распаковка вложенного кортежа

--- Chapter two/tuple.py
+++ Chapter two/tuple.py
@@ -13,5 +13,33 @@ for country, _ in traveler_ids:
 # Cемантика каждого элемента данных определяется его позицией
 
 """Распаковка данных"""
+# Параллельное присваивание
+lax_coordinates = (33.9425, -118.408056)
+latitude, longitude = lax_coordinates  # tuple unpacking
+print(latitude, longitude)
+t = (20, 8)
+print(divmod(*t))
 
+"""Использование * для выборки лишних элементов"""
+a, b, *rest = range(5)
+print(a, b, rest)
+a, b, *rest = range(3)
+print(a, b, rest)
 
+a, *body, c = range(5)
+print(a, body, c)
+
+"""Распаковка вложенного кортежа"""
+metro_areas = [
+	('Tokyo', 'JP', 36.933, (35.689722, 139.691667)),
+	('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889)),
+	('Mexico City', 'MX', 20.142, (19.433333, -99.133333)),
+	('New York-Newark', 'US', 20.104, (40.808611, -74.020386)),
+	('Sao Paulo', 'BR', 19.649, (-23.547778, -46.635833))
+]
+
+print('{:15} | {:^9} | {:^9}'.format('', 'lat.', 'long.'))
+fmt = '{:15} | {:9.4f} | {:9.4f}'
+for name, cc, pop, (latitude, longitude) in metro_areas:
+	if longitude <= 0:
+		print(fmt.format(name, latitude, longitude))
