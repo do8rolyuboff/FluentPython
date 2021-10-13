a995084073cc4087ffc12b63bc21193da5428080
do8rolyuboff <dobniknik@gmail.com>
2021-10-14 00:33:50 +0300

    Update tuple.py
    Закончил с кортежами

--- Chapter two/tuple.py
+++ Chapter two/tuple.py
@@ -43,3 +43,20 @@ fmt = '{:15} | {:9.4f} | {:9.4f}'
 for name, cc, pop, (latitude, longitude) in metro_areas:
 	if longitude <= 0:
 		print(fmt.format(name, latitude, longitude))
+
+# Именованные кортежи
+from collections import namedtuple
+
+City = namedtuple('City', 'name country population coordinates')
+tokyo = City('Tokyo', 'JP', 36.933, (35.689722, 139.691667))
+print(tokyo)
+print(tokyo.population)
+print(type(tokyo))
+print(City._fields)
+
+LatLong = namedtuple('LatLong', 'lat long')
+delhi_data = ('Delhi NCR', 'IN', 21.935, LatLong(28.613889, 77.208889))
+delhi = City._make(delhi_data)  # City(*delhi_data) делает то же самое
+print(delhi._asdict())
+for key, value in delhi._asdict().items():
+	print(key + ':', value)
