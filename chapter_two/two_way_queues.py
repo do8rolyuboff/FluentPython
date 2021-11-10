d6dddf740fb740fcfef54a1361568fe94b1a286d
do8rolyuboff <dobniknik@gmail.com>
2021-11-11 01:09:20 +0300

    Двусторонние и другие очереди
    Конец 2-й главы

---
+++ chapter_two/two_way_queues.py
@@ -0,0 +1,9 @@
+from collections import deque
+
+dq = deque(range(10), maxlen=10)
+print(dq)
+print(dq.rotate(3))
+print(dq.rotate(-4))
+print(dq.appendleft(-1))
+print(dq.extend([11, 22, 33]))
+print(dq.extendleft([10, 20, 30, 40]))
