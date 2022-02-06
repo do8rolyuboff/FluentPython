32491372b1a89c15b36744accd1e4aa80a8ae270
do8rolyuboff <dobniknik@gmail.com>
2022-02-06 23:31:03 +0300

    Create set_theory.py

---
+++ chapter_3/set_theory.py
@@ -0,0 +1,15 @@
+my_list = ['spam', 'spam', 'eggs', 'spam']
+print(set(my_list))
+print(list(set(my_list)))
+
+
+needles = '12345'
+haystack = '123456789'
+
+found = len(set(needles) & set(haystack))
+print(found)
+found = len(set(needles).intersection(haystack))
+print(found)
+
+s = (1,)
+print(type(s))
